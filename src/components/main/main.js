190dc4d7c64dee1e507817d305d638fdd8acccd8
sashasen777 <sashasen@ukr.net>
2020-07-01 21:45:30 +0300

    init

---
+++ src/components/main/main.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import SideDecoration from '../../img/SideDecoration.png';
+import './main.scss';
+
+function Main() {
+  return (
+    <main className='main__info'>
+      <h1 className='main__info__title'>
+        Ресторан "VIVA OLIVE" запрошує всіх бажаючих
+        <br />
+        на обід!
+      </h1>
+      <p className='main__info__text'>
+        Наш ресторан може запропонувати Вам послуги найвищого рівня (літня
+        тераса, затишний закритий зал, домашня атмосфера). Наші шеф-кухарі
+        задовольнять самі витончені запити гурманів. Всі наші страви припадуть
+        до смаку як дітям, так і доросл им шанувальникам вишуканої кухні.
+      </p>
+      <p className='main__info__text'>
+        Гурмани зможуть по достоїнству оцінити всі наші страви!
+      </p>
+      <p className='main__info__text'>
+        Наш ресторан може запропонувати Вам домашній затишок в поєднанні з
+        високою якістю обслуговування і величезним асортиментом цікавих страв.
+      </p>
+      <div className='main__info__image'>
+        <img src={SideDecoration} alt='sideimg' />
+      </div>
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/main/main';
+import Footer from './components/footer/footer';
+
+ReactDOM.render(
+  <div>
+    <Main /> <Footer />
+  </div>,
+  document.getElementById('root')
+);
