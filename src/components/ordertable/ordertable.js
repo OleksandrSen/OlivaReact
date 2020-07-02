183b5b4151fc9582d88b136d8a73cc00472b5de1
sashasen777 <sashasen@ukr.net>
2020-07-02 21:13:15 +0300

    table

--- src/components/ordertable/ordertable.js
+++ src/components/ordertable/ordertable.js
@@ -1,4 +1,24 @@
 import React from 'react';
 import './ordertable.scss';
 
+function Order() {
+  return (
+    <section className='orderTable'>
+      <div className='row'>
+        <h1 className='offset-3 col-6 orderTable__title'>
+          Забронювати
+          <br /> столик
+        </h1>
+      </div>
+      <div className='row'>
+        <div className='col-12 orderTable__main'>
+          <div className='orderTable__main__table'>
+            <div className='orderTable__main__table__num'>1</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
 export default Order;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainHeader from './components/main_header/main_header';
 import Main from './components/main/main';
+import Order from './components/ordertable/ordertable';
 import Footer from './components/footer/footer';
 
 ReactDOM.render(
   <div>
     <MainHeader />
     <Main />
+    <Order />
     <Footer />
   </div>,
   document.getElementById('root')
