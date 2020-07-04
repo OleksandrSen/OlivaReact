218c4b34184c4ffd0fce86b645923d8f2618aa08
YarynaGapanovych <y.gapanovych@gmail.com>
2020-07-04 18:58:15 +0300

    animation

--- src/components/scroll_arrow/scroll_arrow.jsx
+++ src/components/scroll_arrow/scroll_arrow.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { pulse } from 'react-animations';
 import sprite from '../../img/icons-sprite.svg';
 import './scroll_arrow.scss';
+
+const Bounce = styled.div`animation: 3s ${keyframes`${pulse}`} infinite ease `;
 
 function Arrow() {
   return (
     <div className="arrow">
-      <a href="#main" className="arrow__link">
-        <div className="arrow__text">Scroll</div>
-        <svg className="arrow__svg">
-          <use href={sprite + '#arrow'}></use>
-        </svg>
-      </a>
+      <Bounce>
+        <a href="#main" className="arrow__link">
+          <div className="arrow__text">Scroll</div>
+          <svg className="arrow__svg">
+            <use href={sprite + '#arrow'}></use>
+          </svg>
+        </a>
+      </Bounce>
     </div>
   );
 }
