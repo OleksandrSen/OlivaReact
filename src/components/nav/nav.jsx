47614e3216296dfff04db5094e36c6d6c5835e7d
sashasen777 <sashasen@ukr.net>
2020-07-07 16:39:13 +0300

    basket

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import sprite from '../../img/icons-sprite.svg';
+import { Link } from 'react-router-dom';
 import logo from '../../img/logo.png';
 import './nav.scss';
-import Basket from '../basket/basket'
+import Basket from '../basket/basket';
 
 function Nav() {
   return (
@@ -12,29 +11,29 @@ function Nav() {
         <div className='navigation__block row'>
           <ul className='manu manu--left col-5'>
             <li className='manu__item'>
-              <Link to="/main-header" className='manu__link'>
+              <Link to='/main-header' className='manu__link'>
                 Головна
               </Link>
             </li>
             <li className='manu__item'>
-              <Link to="/manu" className='manu__link'>
+              <Link to='/manu' className='manu__link'>
                 Меню
               </Link>
             </li>
             <li className='manu__item'>
-              <Link to="/photo" className='manu__link'>
+              <Link to='/photo' className='manu__link'>
                 Фотогалерея
               </Link>
             </li>
           </ul>
           <ul className='manu manu--right offset-2 col-4'>
             <li className='manu__item'>
-              <Link to="/news" className='manu__link'>
+              <Link to='/news' className='manu__link'>
                 Новини
               </Link>
             </li>
             <li className='manu__item'>
-              <Link to="/contacts" className='manu__link'>
+              <Link to='/contacts' className='manu__link'>
                 Контакти
               </Link>
             </li>
@@ -45,13 +44,6 @@ function Nav() {
           </div>
 
           <Basket />
-          <div className='basket col-1'>
-            <a className='basket__link' href='#s'>
-              <svg className='basket__svg'>
-                <use href={sprite + '#basket'}></use>
-              </svg>
-            </a>
-          </div>
         </div>
       </div>
     </div>
