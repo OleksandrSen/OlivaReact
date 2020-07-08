6e22d3204f07d75267b4e72e441cde9bd774a46a
sashasen777 <sashasen@ukr.net>
2020-07-08 19:57:00 +0300

    app

--- src/index.js
+++ src/index.js
@@ -1,66 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'reset-css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import App from './app';
 
-import Nav from './components/nav/nav';
-import MainHeader from './components/main_header/main_header';
-import Main from './components/main/main';
-import Order from './components/ordertable/ordertable';
-import Footer from './components/footer/footer';
-import Header from './components/header/header';
-import HeaderM from './components/headerM/headerM';
-import HeaderPhoto from './components/header_photo/header_photo';
-
-import News from './components/news/news';
-import Contacts from './components/contacts/contacts';
-import Maps from './components/map/map';
-import MainMenu from './components/main_menu/main_menu';
-import Slider from './components/slider/slider';
-import Season from './components/season/season'
-
-ReactDOM.render(
-  <Router>
-    <div>
-      <Nav />
-      <Route path="/"
-        component={MainHeader}
-        exact />
-      <Route path="/"
-        component={Main}
-        exact />
-      <Route path="/"
-        component={MainMenu}
-        exact />
-      <Route path="/"
-        component={Season}
-        exact />
-      <Route path="/"
-        component={Order}
-        exact />
-
-      <Route path="/main-header" component={MainHeader} />
-      <Route path="/main-header" component={Main} />
-      <Route path="/main-header" component={MainMenu} />
-      {/* <Route path="/main-header" component={Season} /> */}
-      <Route path="/main-header" component={Order} />
-
-      <Route path="/manu" component={HeaderM} />
-      <Route path="/manu" component={MainMenu} />
-      {/* <Route path="/manu" component={Season} /> */}
-
-      <Route path="/photo" component={HeaderPhoto} />
-      <Route path="/photo" component={Slider} />
-
-      <Route path="/news" component={Header} />
-      <Route path="/news" component={News} />
-
-      <Route path="/contacts" component={Header} />
-      <Route path="/contacts" component={Contacts} />
-      <Route path="/contacts" component={Maps} />
-
-      <Footer />
-    </div>
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
