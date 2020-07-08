54a9728dcd26c2d0b208ffe2c2e32b7439337465
YarynaGapanovych <y.gapanovych@gmail.com>
2020-07-08 20:00:47 +0300

    don	 watch this!

--- src/components/basket-form/basket-form.jsx
+++ src/components/basket-form/basket-form.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal'
-import './basket-form.scss'
+// import './basket-form.scss'
 import Confirm from '../confirm/confirm'
 import sprite from '../../img/icons-sprite.svg';
 
@@ -11,7 +11,7 @@ function BasketForm({ BasketFormIsOpen, setBasketFormIsOpen }) {
   const [ConfirmIsOpen, setConfirmIsOpen] = useState(false)
 
   return (
-    <div className="basket-form__modal">
+    <div classNameName="basket-form__modal">
       <Modal
         isOpen={BasketFormIsOpen} onRequestClose={() => setBasketFormIsOpen(false)}
         style={
@@ -26,61 +26,56 @@ function BasketForm({ BasketFormIsOpen, setBasketFormIsOpen }) {
         <section className="basket-form container">
           <div className="row">
             <h3 className="basket-form__caption col-12">Кошик</h3>
-
-            <h4 className="basket-form__title col-5">Страва</h4>
-            <h4 className="basket-form__title col-3">Кількість</h4>
-            <h4 className="basket-form__title col-3">Ціна</h4>
-
-            <ul className="basket-form__list col-5">
-              <li className="basket-form__meal col-12">Італійські м'ясні делікатеси</li>
-              <li className="basket-form__meal col-12">Брускети з в'яленими томатами</li>
-            </ul>
-
-            <ul className="basket-form__amount col-3">
-              <li className="basket-form__amount-item">
-                <button className="basket-form__btn">-</button>
-                <div className="basket-form__number">1</div>
-                <button className="basket-form__btn">+</button>
-              </li>
-              <li className="basket-form__amount-item">
-                <button className="basket-form__btn">-</button>
-                <div className="basket-form__number">1</div>
-                <button className="basket-form__btn">+</button>
-              </li>
-            </ul>
-
-            <ul className="basket-form__cost col-3">
-              <li>119 грн.</li>
-              <li>51 грн.</li>
-            </ul>
-
-            <ul className="basket-form__delete col-1">
-              <li>
-                <button className="basket-form__btn">x</button>
-              </li>
-              <li>
-                <button className="basket-form__btn">x</button>
-              </li>
-            </ul>
-
-            <div className="basket-form__sum col-5">Сума замовлення:
-				<span className="basket-form__sum--bold">170грн</span></div>
-
-            <button className="basket-form__button offset-3 col-4"
-              onClick={() => setConfirmIsOpen(true)}> Замовити</button>
-
-            <svg class="basket-form__cross" onClick={() => setBasketFormIsOpen(false)}>
+            <table className="table col-12">
+              <tbody>
+                <tr>
+                  <th className="basket-form__title basket-form__title--meal">Страва</th>
+                  <th className="basket-form__title basket-form__title--amount">Кількість</th>
+                  <th className="basket-form__title basket-form__title--cost">Ціна</th>
+                  <td className="basket-form__title--delete"></td>
+                </tr>
+                <tr>
+                  <td className="basket-form__meal">Італійські м'ясні делікатеси</td>
+                  <td className="basket-form__amount">
+                    <button className="basket-form__btn">-</button>
+                    <div className="basket-form__number">1</div>
+                    <button className="basket-form__btn">+</button>
+                  </td>
+                  <td>119 грн.</td>
+                  <td><button className="basket-form__btn-delete">x</button></td>
+                </tr>
+                <tr>
+                  <td className="basket-form__meal">Брускети з в'яленими томатами</td>
+                  <td className="basket-form__amount">
+                    <button className="basket-form__btn">-</button>
+                    <div className="basket-form__number">1</div>
+                    <button className="basket-form__btn">+</button>
+                  </td>
+                  <td>51 грн.</td>
+                  <td><button className="basket-form__btn-delete">x</button></td>
+                </tr>
+                <tr>
+                  <td colspan="2" className="basket-form__sum">
+                    Сума замовлення:
+						<span className="basket-form__sum--bold">170грн</span>
+                  </td>
+                  <td colspan="2"><button className="basket-form__button"
+                    onClick={() => setConfirmIsOpen(true)}>Замовити</button></td>
+                </tr>
+              </tbody>
+            </table>
+            <svg className="basket-form__cross" onClick={() => setBasketFormIsOpen(false)}>
               <use href={sprite + '#cross'}></use>
             </svg>
-          </div>
 
-          <Confirm
-            ConfirmIsOpen={ConfirmIsOpen}
-            setConfirmIsOpen={setConfirmIsOpen}
-            setBasketFormIsOpen={setBasketFormIsOpen} />
+            <Confirm
+              ConfirmIsOpen={ConfirmIsOpen}
+              setConfirmIsOpen={setConfirmIsOpen}
+              setBasketFormIsOpen={setBasketFormIsOpen} />
+          </div>
         </section>
-      </Modal>
-    </div>
+      </Modal >
+    </div >
   )
 }
 
