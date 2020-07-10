f334d367f2682e26e714f223bd7fec54907d6866
YarynaGapanovych <y.gapanovych@gmail.com>
2020-07-10 11:43:22 +0300

    navLink

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,50 +6,52 @@ import './footer.scss';
 function Footer() {
   return (
     <footer className='footer'>
-      <div className='row'>
-        <div className='col-4 footer__copyright'>
-          При використанні матеріалів з сайту посилання на ресурс
+      <div className='container'>
+        <div className='row'>
+          <div className='col-4 footer__copyright'>
+            При використанні матеріалів з сайту посилання на ресурс
           <br />
           обов'язкове
           <a href='http://viv-live.com.ua/poltava/' className='webSite'>
-            www.viv-live.com.ua
+              www.viv-live.com.ua
           </a>
-        </div>
-        <div className='col-4 footer__logo'>
-          <img src={logo} alt='logo' />
-        </div>
-        <div className='col-4 footer__social'>
-          <div className='row footer__social__text'>
-            Також нас можна знайти в<br />
+          </div>
+          <div className='col-4 footer__logo'>
+            <img src={logo} alt='logo' />
+          </div>
+          <div className='col-4 footer__social'>
+            <div className='row footer__social__text'>
+              Також нас можна знайти в<br />
             соціальних мережах,
             <br />
             приєднуйтесь до нас:
           </div>
-          <div className='row footer__social__icons'>
-            <div className='svgBg'>
-              <svg className='socialIcon'>
-                <use href={sprite + '#instagram'}></use>
-              </svg>
-            </div>
-            <div className='svgBg'>
-              <svg className='socialIcon'>
-                <use href={sprite + '#vk'}></use>
-              </svg>
-            </div>
-            <div className='svgBg'>
-              <svg className='socialIcon'>
-                <use href={sprite + '#twitter'}></use>
-              </svg>
-            </div>
-            <div className='svgBg'>
-              <svg className='socialIcon'>
-                <use href={sprite + '#facebook'}></use>
-              </svg>
-            </div>
-            <div className='svgBg'>
-              <svg className='socialIcon'>
-                <use href={sprite + '#odnoklassniki'}></use>
-              </svg>
+            <div className='row footer__social__icons'>
+              <div className='svgBg'>
+                <svg className='socialIcon'>
+                  <use href={sprite + '#instagram'}></use>
+                </svg>
+              </div>
+              <div className='svgBg'>
+                <svg className='socialIcon'>
+                  <use href={sprite + '#vk'}></use>
+                </svg>
+              </div>
+              <div className='svgBg'>
+                <svg className='socialIcon'>
+                  <use href={sprite + '#twitter'}></use>
+                </svg>
+              </div>
+              <div className='svgBg'>
+                <svg className='socialIcon'>
+                  <use href={sprite + '#facebook'}></use>
+                </svg>
+              </div>
+              <div className='svgBg'>
+                <svg className='socialIcon'>
+                  <use href={sprite + '#odnoklassniki'}></use>
+                </svg>
+              </div>
             </div>
           </div>
         </div>
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,88 +1,42 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../../img/logo.png';
 import './nav.scss';
 import Basket from '../basket/basket';
 
 function Nav() {
-  const [ItemMainClicked, setItemMainClicked] = useState(false)
-  const [ItemMenuClicked, setItemMenuClicked] = useState(false)
-  const [ItemPhotoClicked, setItemPhotoClicked] = useState(false)
-  const [ItemNewsClicked, setItemNewsClicked] = useState(false)
-  const [ItemContactsClicked, setItemContactsClicked] = useState(false)
-
 
   return (
     <div className='navigation'>
       <div className='container'>
         <div className='navigation__block row'>
           <ul className='manu manu--left col-5'>
-            <li className={ItemMainClicked ? 'manu__item--active' : 'manu__item'}
-              onClick={() => {
-                setItemMainClicked(true)
-                setItemMenuClicked(false)
-                setItemPhotoClicked(false)
-                setItemNewsClicked(false)
-                setItemContactsClicked(false)
-              }}
-              isOpen={ItemMainClicked}>
-              <Link to='/main-header' className='manu__link'>
+            <li className='manu__item'>
+              <NavLink activeClassName='manu__link--active' className='manu__link' to='/main-header'>
                 Головна
-              </Link>
+              </NavLink>
             </li>
-            <li className={ItemMenuClicked ? 'manu__item--active' : 'manu__item'}
-              onClick={() => {
-                setItemMenuClicked(true)
-                setItemMainClicked(false)
-                setItemPhotoClicked(false)
-                setItemNewsClicked(false)
-                setItemContactsClicked(false)
-              }}
-              isOpen={ItemMenuClicked}>
-              <Link to='/manu' className='manu__link'>
+            <li className='manu__item'>
+              <NavLink activeClassName='manu__link--active' className='manu__link' to='/manu' >
                 Меню
-              </Link>
+              </NavLink>
             </li>
-            <li className={ItemPhotoClicked ? 'manu__item--active' : 'manu__item'}
-              onClick={() => {
-                setItemPhotoClicked(true)
-                setItemMenuClicked(false)
-                setItemMainClicked(false)
-                setItemNewsClicked(false)
-                setItemContactsClicked(false)
-              }}
-              isOpen={ItemPhotoClicked}>
-              <Link to='/photo' className='manu__link'>
+            <li className='manu__item'>
+              <NavLink to='/photo' activeClassName='manu__link--active' className='manu__link'>
                 Фотогалерея
-              </Link>
+              </NavLink>
             </li>
           </ul>
           <ul className='manu manu--right offset-2 col-4'>
-            <li className={ItemNewsClicked ? 'manu__item--active' : 'manu__item'}
-              onClick={() => {
-                setItemNewsClicked(true)
-                setItemPhotoClicked(false)
-                setItemMenuClicked(false)
-                setItemMainClicked(false)
-                setItemContactsClicked(false)
-              }}
-              isOpen={ItemNewsClicked}>
-              <Link to='/news' className='manu__link'>
+            <li className='manu__item'>
+              <NavLink to='/news' activeClassName='manu__link--active' className='manu__link'>
                 Новини
-              </Link>
+              </NavLink>
             </li>
-            <li className={ItemContactsClicked ? 'manu__item--active' : 'manu__item'}
-              onClick={() => {
-                setItemContactsClicked(true)
-                setItemNewsClicked(false)
-                setItemPhotoClicked(false)
-                setItemMenuClicked(false)
-                setItemMainClicked(false)
-              }}
-              isOpen={ItemContactsClicked}>
-              <Link to='/contacts' className='manu__link'>
+            <li className='manu__item'>
+              <NavLink to='/contacts' activeClassName='manu__link--active' className='manu__link'>
                 Контакти
-              </Link>
+              </NavLink>
             </li>
           </ul>
 
