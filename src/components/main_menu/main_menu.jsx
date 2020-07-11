13824c7ff6104cabc64ace2408e2f0e2bddab5ca
sashasen777 <sashasen@ukr.net>
2020-07-11 09:28:07 +0300

    orderTableModal

--- src/components/main_menu/main_menu.jsx
+++ src/components/main_menu/main_menu.jsx
@@ -1,120 +1,122 @@
 import React from 'react';
 import sprite from '../../img/icons-sprite.svg';
 import './main_menu.scss';
-import MenuNav from './menu_nav/menu_nav'
-import MenuHeaderRow from './menu_header-row/menu_header-row'
+import MenuNav from './menu_nav/menu_nav';
+import MenuHeaderRow from './menu_header-row/menu_header-row';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function MainMenu() {
-
   const appetizer = [
     {
       meal: 'Прошутто крудо з грушею',
       desk: '',
       weight: 165,
-      cost: 71
+      cost: 71,
     },
     {
       meal: "Італійські м'ясні делікатеси",
       desk: '(прошутто крудо, брезаола, салямі, салямі гостра)',
       weight: 120,
-      cost: 119
+      cost: 119,
     },
     {
       meal: 'Сирна палітра',
       desk: '(сир Пармезан, сир Брі, сир Горгонзола, сир Скаморція)',
       weight: 205,
-      cost: 121
+      cost: 121,
     },
     {
-      meal: "Карпаччо із лосося",
+      meal: 'Карпаччо із лосося',
       desk: '(з грінками)',
       weight: 110,
-      cost: 99
+      cost: 99,
     },
     {
       meal: 'Тартар із лосося',
       desk: '(з грінками)',
       weight: 160,
-      cost: 119
+      cost: 119,
     },
     {
-      meal: "Антіпасті на двох",
+      meal: 'Антіпасті на двох',
       desk: "(італійські м'ясні та сирні делікатеси)",
       weight: 390,
-      cost: 248
+      cost: 248,
     },
     {
       meal: 'Брускети з соусом із консервованого тунця',
       desk: '(подаються з овочами)',
       weight: 160,
-      cost: 51
+      cost: 51,
     },
     {
-      meal: "Брускети з паштетом з гусячої печінки",
+      meal: 'Брускети з паштетом з гусячої печінки',
       desk: '(під вишневим соусом)',
       weight: 140,
-      cost: 61
+      cost: 61,
     },
     {
       meal: "Брускети з в'яленими томатами",
       desk: '',
       weight: 100,
-      cost: 51
+      cost: 51,
     },
     {
-      meal: "Вітелло тоннато",
+      meal: 'Вітелло тоннато',
       desk: '(тонко нарізана телятина під соусом із тунця)',
       weight: 130,
-      cost: 69
-    }
-  ]
+      cost: 69,
+    },
+  ];
 
   const salad = [
     {
       meal: 'Олів’є з курятиною, шинкою та молодим горошком',
       desk: '',
       weight: 250,
-      cost: 145
+      cost: 145,
     },
     {
       meal: "Салат Романо Houston's",
       desk: '(з домашнім курчам-гриль і арахісовим соусом)',
       weight: 300,
-      cost: 165
+      cost: 165,
     },
     {
-      meal: 'Овочевий салат із булгуром, кінзою, зеленим соусом та гарбузовим насінням',
+      meal:
+        'Овочевий салат із булгуром, кінзою, зеленим соусом та гарбузовим насінням',
       desk: '',
       weight: 200,
-      cost: 115
+      cost: 115,
     },
     {
-      meal: "Цуккіні",
-      desk: '(карпачо з базиліком, рікотою, гарбузовим насінням та соусом горгонзола)',
+      meal: 'Цуккіні',
+      desk:
+        '(карпачо з базиліком, рікотою, гарбузовим насінням та соусом горгонзола)',
       weight: 160,
-      cost: 115
+      cost: 115,
     },
     {
       meal: 'Ризото з лисичками та пармезаном ',
       desk: '',
       weight: 250,
-      cost: 250
+      cost: 250,
     },
     {
-      meal: "Зелений салат",
-      desk: "(із молодим шпинатом, руколою, свіжою м’ятою, грейпфрутом і креветками під чилі- соусом. )",
+      meal: 'Зелений салат',
+      desk:
+        '(із молодим шпинатом, руколою, свіжою м’ятою, грейпфрутом і креветками під чилі- соусом. )',
       weight: 200,
-      cost: 265
-    }
-  ]
+      cost: 265,
+    },
+  ];
 
   const Appetizer = ({ appetizer }) => (
     <tbody>
       <MenuHeaderRow />
-      {appetizer.map(item => (
-        <tr className='listRow' key={item.appetizer} >
+      {appetizer.map((item) => (
+        <tr className='listRow' key={item.appetizer}>
           <td className='firstCol'>
             <h4 className='food__name'>{item.meal}</h4>
             {item.desk}
@@ -133,13 +135,13 @@ function MainMenu() {
         </tr>
       ))}
     </tbody>
-  )
+  );
 
   const Salad = ({ salad }) => (
     <tbody>
       <MenuHeaderRow />
-      {salad.map(item => (
-        <tr className='listRow' key={item.salad} >
+      {salad.map((item) => (
+        <tr className='listRow' key={item.salad}>
           <td className='firstCol'>
             <h4 className='food__name'>{item.meal}</h4>
             {item.desk}
@@ -158,9 +160,7 @@ function MainMenu() {
         </tr>
       ))}
     </tbody>
-  )
-
-
+  );
 
   return (
     <section className='menu' id='menu'>
@@ -171,28 +171,27 @@ function MainMenu() {
         <div className='offset-4 col-8'>
           <MenuNav />
           <table className='menu__list'>
-            <Route path="/" component={Appetizer} exact>
+            <Route path='/' component={Appetizer} exact>
               <Appetizer appetizer={appetizer} />
             </Route>
-            <Route path="/" salad={salad} exact>
+            <Route path='/' salad={salad} exact>
               <Salad salad={salad} />
             </Route>
-            <Route path="/main-header/a" component={Appetizer}>
+            <Route path='/main-header/a' component={Appetizer}>
               <Appetizer appetizer={appetizer} />
             </Route>
-            <Route path="/main-header/s" salad={salad}>
+            <Route path='/main-header/s' salad={salad}>
               <Salad salad={salad} />
             </Route>
 
-            <Route path="/menu/a" component={Appetizer}>
+            <Route path='/menu/a' component={Appetizer}>
               <Appetizer appetizer={appetizer} />
             </Route>
-            <Route path="/menu/s" salad={salad}>
+            <Route path='/menu/s' salad={salad}>
               <Salad salad={salad} />
             </Route>
 
             {/* <Salad salad={salad} /> */}
-
           </table>
         </div>
       </div>
@@ -204,5 +203,3 @@ function MainMenu() {
 }
 
 export default MainMenu;
-
-
--- src/components/ordertable/thankorder/thankorder.jsx
+++ src/components/ordertable/thankorder/thankorder.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
+import Modal from 'react-modal';
 import register from '../../../img/thankorder.png';
 import Cross from '../../cross/cross';
 import './thankorder.scss';
 
-function ThankOrder() {
+Modal.setAppElement('#root');
+
+function ThankOrder({ ThankIsOpen, setThankIsOpen, setTableFormIsOpen }) {
   return (
-    <section className='thankorder'>
-      <div className='container'>
-        <Cross />
-        <div className='thankorder__icon'>
-          <img src={register} alt='thankicon' />
-        </div>
-        <h3 className='thankorder__title'>
-          Дякуємо, столик
-          <br /> заброньовано!
-        </h3>
-      </div>
-    </section>
+    <div>
+      <Modal
+        isOpen={ThankIsOpen}
+        onRequestClose={() => {
+          setThankIsOpen(false);
+          setTableFormIsOpen(false);
+        }}
+        style={{
+          content: {
+            width: '550px',
+            height: '350px',
+            margin: '0 auto',
+            borderRadius: '15px',
+            overflow: 'hidden',
+          },
+        }}
+      >
+        <section className='thankorder'>
+          <div className='container'>
+            <Cross
+              onClick={() => {
+                setThankIsOpen(false);
+                setTableFormIsOpen(false);
+              }}
+            />
+            <div className='thankorder__icon'>
+              <img src={register} alt='thankicon' />
+            </div>
+            <h3 className='thankorder__title'>
+              Дякуємо, столик
+              <br /> заброньовано!
+            </h3>
+          </div>
+        </section>
+      </Modal>
+    </div>
   );
 }
 
